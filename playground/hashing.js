28485998ee582ea62b06a93310736bd6be956ba0
Lahiru-Weerasekara <weerasekara1121@gmial.com>
2018-02-21 11:57:50 +0530

    Hash user password before saving

--- playground/hashing.js
+++ playground/hashing.js
@@ -1,7 +1,24 @@
 const {SHA256} = require('crypto-js');
 const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
 
-var data = {
+var password = '123abc!';
+
+/*bcrypt.genSalt(10, (err, salt) => {
+    bcrypt.hash(password, salt, (err, hash) => {
+        console.log(hash);
+    });
+});*/
+
+var hashedPassword = '$2a$10$UURgcrDGDnqYhb/TLvuYl.qWaFYJ5KlJSYerjwQ67jHGx0VztsmD2';
+
+bcrypt.compare(password, hashedPassword, (err, res) => {
+    console.log(res);
+});
+
+
+
+/*var data = {
     id: 10
 };
 
@@ -9,7 +26,8 @@ var token = jwt.sign(data, '123abc');
 console.log(token);
 
 var decoded = jwt.verify(token, '123abc');
-console.log('decoded', decoded);
+console.log('decoded', decoded);*/
+
 
 // var message = 'I am user number 3';
 // var hash = SHA256(message).toString();
